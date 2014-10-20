97d585f6e725c72f861fa28cc3875862170e6b64
Michelle Merriweather <mmerriweather@mccicorp.com>
2014-10-20 10:47:17 -0400

    deleted app folder in branch

--- app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    if session['devise.google_data']
-      puts session['devise.google_data']['credentials']
-      token = session['devise.google_data']['credentials']['token']
-      @events = GoogleApi.new.fetch_events(token)
-    end
-  end
-end
