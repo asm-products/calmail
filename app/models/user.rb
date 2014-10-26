6b0afe025d64d210fc27b7606f0d6c6b429d6fe5
Rafael Borochov <rafael.borochov@gmail.com>
2014-10-26 11:33:28 +0200

    User Time Preference

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   devise :omniauthable, :omniauth_providers => [:google_oauth2]
+  validate :must_be_valid_preferred_time
   def self.find_for_google_oauth2(omniauth, signed_in_resource=nil)
     data = omniauth.info
     user = User.where(:email => data['email']).first
@@ -16,10 +17,17 @@ class User < ActiveRecord::Base
     user
   end
 
+  def must_be_valid_preferred_time
+    unless self.prefered_time.min % 15 == 0
+      errors.add(:type, "Prefered time must by an increment of 15 minutes")
+    end
+  end
+
   def self.send_daily_emails
     # TODO: Filter to select specific users
     # TODO: Add model to schedule/track emails
-    User.all.each { |user| user.send_daily_email }
+    #User.all.each { |user| user.send_daily_email }
+    User.where(preferred_time: Time.now).each { |user| user.send_daily_email }
   end
 
   def send_daily_email
