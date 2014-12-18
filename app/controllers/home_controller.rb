c63a837e26bb9eb407679e58c6acaf09dc9f1dfe
Przemek Mroczek <przemyslawmroczek@o2.pl>
2014-12-18 20:41:02 +0100

    Remove binding.pry

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def index
-    binding.pry
-
     if session['devise.google_data']
       token = session['devise.google_data']['credentials']['token']
       @events = GoogleApi.new.fetch_events(token)
