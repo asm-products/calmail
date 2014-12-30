14c5dfefd97a0be7d44b2c0cd2d59c44aebf3246
Przemek Mroczek <przemyslawmroczek@o2.pl>
2014-12-30 17:23:42 +0100

    FIll information about event

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -2,8 +2,15 @@ class EventDecorator < Draper::Decorator
   delegate_all
 
   def attendees_names
+    return 'No attendees' if attendees.blank?
     object.attendees.map do |attendee|
-      attendee['display_name']
-    end.join(', ')
+      attendee['displayName']
+    end.compact.join(', ')
+  end
+
+  def held_between_hours
+    start_formatted = event.starts_at.strftime("%H:%M")
+    end_formatted = event.ends_at.strftime("%H:%M")
+    "#{start_formatted} - #{end_formatted}"
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: 'from@example.com'
+  add_template_helper ApplicationHelper
 
   def daily_digest_email(user)
     @user = user
-    @events = user.events
+    @events = EventDecorator.decorate_collection(user.events)
     mail(
       to: @user.email,
       subject: 'Calmail Daily Summary'
